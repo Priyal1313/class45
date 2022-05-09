a6b30ec00bb1a36b6433e157c0a14a93c3cc00f6
Priyal1313 <86111883+Priyal1313@users.noreply.github.com>
2022-05-09 16:52:07 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,6 +2,12 @@ var bg,bgImg;
 var player, shooterImg, shooter_shooting;
 var bulletSound;
 var zombieImg;
+var zombieGroup;
+var bullet,bulletGroup;
+var life=3;
+var bullets=50;
+var score =0;
+
 function preload(){
   
   shooterImg = loadImage("shooter_2.png")
@@ -9,6 +15,7 @@ function preload(){
 zombieImg = loadImage ("zombie.png")
   bgImg = loadImage("bg.jpeg")
 bulletSound = loadSound("explosion.mp3")
+lost  = loadSound("lose.mp3")
 }
 
 function setup() {
@@ -29,7 +36,8 @@ player = createSprite(displayWidth-1150, displayHeight-300, 50, 50);
    player.debug = true
    player.setCollider("rectangle",0,0,300,300)
 
-
+zombieGroup = new Group();
+bulletGroup = new Group();
 }
 
 function draw() {
@@ -49,19 +57,48 @@ console.log(frameCount);
 
 //release bullets and change the image of shooter to shooting position when space is pressed
 if(keyWentDown("space")){
- 
+ bullet=createSprite(50,player.y-30,20,10);
+ bullet.velocityX = 50;
+ bulletGroup.add(bullet);
+ bullets = bullets-1;
   player.addImage(shooter_shooting)
  bulletSound.play();
 }
 
 //player goes back to original standing image once we stop pressing the space bar
 else if(keyWentUp("space")){
+
   player.addImage(shooterImg)
+}
+//to destroy the zombie when bullet touches the zombie
+if(zombieGroup.isTouching(bulletGroup)){
+for(var i = 0;i<zombieGroup.length;i++){
+if(zombieGroup[i].isTouching(bulletGroup)){
+zombieGroup[i].destroy()
+bulletGroup.destroyEach()
+bulletSound.play()
+score=score+1
+}
+}
+}
+//to destroy zombie when zombie touches the player
+if(zombieGroup.isTouching(player)){
+lost.play();
+for(var i = 0;i<zombieGroup.length;i++){
+  if(zombieGroup[i].isTouching(player)){
+  zombieGroup[i].destroy()
+  life = life-1;
+  }
+}
 }
 spawnZombies()
 
 drawSprites();
-
+textSize(20);
+fill("white");
+text("Bullets= "+bullets,displayWidth-200,50);
+text("Score= "+score,displayWidth-200,100);
+text("Lives= "+life,displayWidth-200,150);
 }
 
 
@@ -71,8 +108,8 @@ var zombie = createSprite(width,Math.round(random(50,height-50)),50,100);
 zombie.addImage(zombieImg);
 zombie.scale = 0.1;
 zombie.velocityX=-3;
-
-
+zombie.lifetime=400;
+zombieGroup.add(zombie)
 
 }
 
